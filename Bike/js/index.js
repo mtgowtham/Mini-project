c3117fa53dea4363bbb68bcdd4921a228fa85843
mtgowtham <155419418+mtgowtham@users.noreply.github.com>
2024-01-18 06:11:46 +0000

    Add files via upload

---
+++ Bike/js/index.js
@@ -0,0 +1,24 @@
+function openNav(){
+    document.getElementById("myNav").classList.toggle("menu-width");
+    document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style");
+
+}
+$(".owl-carousel").owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    navText:[],
+    autoplay:true,
+    autoplayHoverPause : true,
+    responsive : {
+        0:{
+            items : 1
+        },
+        600:{
+            items : 2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
